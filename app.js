c19c6fe6d3e520b0d5574be62ac4d74f6dd980c9
Jaiwincr7 <chaudharijr@rknec.edu>
2023-11-05 12:41:13 +0530

    asd

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ require("dotenv").config()
 const express = require("express");
 
 const app= express()
-
+const cors=require("cors")
+app.use(cors({
+    origin:"*",
+    method:["GET","POST"]
+}))
 const connectDB= require("./db/connect")
 
 const PORT= process.env.PORT || 5000;
